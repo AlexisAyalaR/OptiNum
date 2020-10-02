41b1c178aa3085bc652b28b742b598988d73779c
Andres Cruz <andrscyv@gmail.com>
2020-10-02 17:41:08 -0500

    Metodo completo pero no funciona

--- descenso2pasos.m
+++ descenso2pasos.m
@@ -4,18 +4,32 @@ function [W, H] = descenso2pasos(X, k)
 [r, p] = size(X);
 W = ones(r, k);
 H = ones(k, p);
-maxiter = 300;
+maxiter = 50;
 
 for i = 1:maxiter
+    %Resolvemos problema cuadrárico para W fija
     for n = 1:p
         col_n_x = X(:, n);
-        Q = W'*W;
-        c = -col_n_x'*W;
-        b = zeros(r,1);
+        Q = W'*W; % dim k*k
+        c = (-col_n_x'*W)';
+        b = zeros(k,1);
+        A = eye(k);
         [x, y, mu] = punintpc(Q,A,c,b);
         H(:,n) = x;
+        
     end
-
+    %Resolvemos para H fija
+    for m = 1:r
+        renglon_m_x = X(m, :);%dim 1*p
+        Q = H*H';%dim k*k
+        c = (-renglon_m_x*H')'; %dim (1*p*p*k)' = k*1
+        b = zeros(k,1);
+        A = eye(k);
+        [x, y, mu] = punintpc(Q,A,c,b);
+        W(m,:) = x';
+        
+    end
+    i
 end
 
 end
---
+++ payaso.m
@@ -0,0 +1,7 @@
+% clear all;
+% clc;
+% load clown;
+% colormap('gray');
+%image(X);
+X = [ 1 1 1; 2 2 2 ]
+[W, H] = descenso2pasos(X, 5);
